696c554146a24f02689e7819c51f6512887a7bdc
Jeya Keerthana <jeya.keerthana@vimix.app>
2026-02-05 16:43:45 +0530

    userprofile model created then registered in admin panel and index page created

--- backend/Ecommerce/store/models.py
+++ backend/Ecommerce/store/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Userprofile(models.Model):
+    name = models.CharField(max_length=150)
+    email = models.CharField(max_length=200)
+    password = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=30)
+    linkedin = models.URLField(max_length=500, null=True, blank=True)
+    
+    def __str__(self):
+        return self.name
