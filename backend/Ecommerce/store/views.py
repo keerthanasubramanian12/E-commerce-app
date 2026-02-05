9bafbdb11c7f73e0e0e28cd8301a9d6dd9feba44
Jeya Keerthana <jeya.keerthana@vimix.app>
2026-02-05 18:15:59 +0530

    signup and login api created

--- backend/Ecommerce/store/views.py
+++ backend/Ecommerce/store/views.py
@@ -1,5 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Userprofile
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+class SignupView(APIView):
+    
+    # Create a new user
+    def post(self, request):
+        name = request.data.get('name')
+        email = request.data.get('email')
+        password = request.data.get('password')
+        phone_number = request.data.get('phone_number')
+        confirm_password = request.data.get('confirm_password')
+        linkedin = request.data.get('linkedin')
+        
+        if password != confirm_password:
+            return Response({'error': 'Passwords do not match'}, status=400)
+
+        if Userprofile.objects.filter(email=email).exists():
+            return Response({'error': 'Email already exists'}, status=400)
+
+        user = Userprofile(name=name, email=email, password=password, phone_number=phone_number, linkedin=linkedin)
+        user.save()
+
+        return Response({'message': 'User created successfully'}, status=201)
+    
+class LoginView(APIView):
+    def post(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+        
+        user = Userprofile.objects.filter(email=email, password=password).first()
+        
+        if user:
+            return Response({'message': 'Login successful'}, status=200)
+        else:
+            return Response({'error': 'Invalid email or password'}, status=401)
+    
